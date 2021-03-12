0077d3c3aa068d6404786863ac7cb08e7f2e8eaf
zxc8612253 <71476349+zxc8612253@users.noreply.github.com>
2021-03-12 11:54:51 +0800

    ui未完成

---
+++ 55688/Assets/scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("等級")]
+    [Tooltip("這是角色的等級")]
+    public int lv = 1;
+    [Header("移動速度"), Range(0, 300)]
+    public float speed = 10.5f;
+    public bool isDead = false;
+    [Tooltip("這是角色的名字")]
+    public string cName = "貓咪";
+}
